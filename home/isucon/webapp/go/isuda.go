5ded8e8cc7bbb0b8384709dcfd826086c6e13685
izumin5210 <masayuki@izumin.info>
2017-10-21 09:10:07 +0900

    Use Replace() instead of regex

--- home/isucon/webapp/go/isuda.go
+++ home/isucon/webapp/go/isuda.go
@@ -14,7 +14,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -88,6 +87,8 @@ func initializeHandler(w http.ResponseWriter, r *http.Request) {
 		saveEntry(e)
 	}
 	rows.Close()
+
+	updateReplacer()
 
 	resp, err := http.Get(fmt.Sprintf("%s/initialize", isutarEndpoint))
 	panicIf(err)
@@ -163,6 +164,7 @@ func keywordPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	saveEntry(&Entry{Keyword: keyword, Description: description})
+	updateReplacer()
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
@@ -291,6 +293,7 @@ func keywordByKeywordDeleteHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	deleteEntry(e)
+	updateReplacer()
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
@@ -298,20 +301,7 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 	if content == "" {
 		return ""
 	}
-	keywords := getKeywordsOrderByLength()
-	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
-	kw2sha := make(map[string]string)
-	content = re.ReplaceAllStringFunc(content, func(kw string) string {
-		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		return kw2sha[kw]
-	})
-	content = html.EscapeString(content)
-	for kw, hash := range kw2sha {
-		u, err := r.URL.Parse(baseUrl.String() + "/keyword/" + pathURIEscape(kw))
-		panicIf(err)
-		link := fmt.Sprintf("<a href=\"%s\">%s</a>", u, html.EscapeString(kw))
-		content = strings.Replace(content, hash, link, -1)
-	}
+	content = replace(html.EscapeString(content))
 	return strings.Replace(content, "\n", "<br />\n", -1)
 }
 
